669fe85fc8d2c0de40d6ea485dedee42163d1586
wilcom1 <wilmerunal@gmail.com>
2016-12-11 10:19:14 -0500

    Funcionalidad de reservas completamente funcional. Las reservas se guardan en la tabla de reservas.

--- TouristicPlaces/reservas.aspx.vb
+++ TouristicPlaces/reservas.aspx.vb
@@ -12,99 +12,94 @@ Public Class reservas
         'de texto de tbUserID
         tbUserID.Text = Context.User.Identity.Name.ToString
 
-        'al cargar la pagina
-        'tome el valor de ulr id_lugar y pongalo en
-        tblugarID.Text = Request.QueryString("id_lugar").ToString
-
-        If (IsPostBack) Then ' si la pagina esta siendo emitida con datos y no cargada por primera vez.
-
-
-
-            'Try
-            '    'definimos la variable de conexion y el string de conexion a utilizar dentro de ella
-            '    Dim conn As New SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings("cs").ConnectionString)
+        
+        Try
+            'al cargar la pagina
+            'tome el valor de ulr id_lugar y pongalo en    
+            tblugarID.Text = Request.QueryString("id_lugar").ToString
+        Catch ex As Exception
+            tblugarID.Text = ""
+        End Try
 
 
-            '    conn.Open() ' abrimos la conexion
+        'If (IsPostBack) Then ' si la pagina esta siendo emitida con datos y no cargada por primera vez.
+
+        'Try
+        '    'definimos la variable de conexion y el string de conexion a utilizar dentro de ella
+        'Dim conn As New SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings("ApplicationServices").ConnectionString)
 
 
-            '    'definimos nuestro query: numero de usuarios en la base de datos que aparecen con el usuario ingresado en la pagina de registro
-            '    Dim confirmarUsuario As String = "Select Count(*) from usuarios where usuario =  '" + tbUsuario.Text + "'"
+        'conn.Open() ' abrimos la conexion
 
 
-            '    'definimos el commando a ejecutar y pasamos el query en el y la conexion a utiliar para ejecutar el comando
-            '    Dim command As SqlCommand = New SqlCommand(confirmarUsuario, conn)
+        '    'definimos nuestro query: numero de usuarios en la base de datos que aparecen con el usuario ingresado en la pagina de registro
 
 
-            '    'definimos una variable de entero para guardar el resultado del query
-            '    Dim numDeUsuarios = Convert.ToInt32(command.ExecuteScalar.ToString())
+        'Dim confirmarUsuario As String = "Select Count(*) from usuarios where UserId =  '" + tbUserID.Text + "'"
 
 
-            '    If (numDeUsuarios = 1) Then
-            '        MsgBox("El usuario ya existe")
-            '    End If
+        '    'definimos el commando a ejecutar y pasamos el query en el y la conexion a utiliar para ejecutar el comando
+        'Dim command As SqlCommand = New SqlCommand(confirmarUsuario, conn)
 
 
-            '    conn.Close() 'cerramos la conexion
-            'Catch ex As Exception
-            '    MsgBox("error: " + ex.ToString())
-            'End Try
+        '    'definimos una variable de entero para guardar el resultado del query
+        'Dim numDeUsuarios = Convert.ToInt32(command.ExecuteScalar.ToString())
+
+
+        'If (numDeUsuarios = 1) Then
+        'MsgBox("El usuario ya existe")
+        'End If
+        '
+
+        'conn.Close() 'cerramos la conexion
+        'Catch ex As Exception
+        '        MsgBox("error: " + ex.ToString())
+        'End Try
 
 
 
-        End If
+        '    End If
     End Sub
 
 
     Protected Sub btnReservar_Click(sender As Object, e As EventArgs) Handles btnReservar.Click
 
 
-        'Try
-        '    Dim id As Guid = Guid.NewGuid()
+        Try
+            Dim id As Guid = Guid.NewGuid()
+            'definimos la variable de conexion y el string de conexion a utilizar dentro de ella
+
+            Dim conn As New SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings.Item("ConnectionString").ToString)
+            conn.Open() ' abrimos la conexion
+
+            'definimos nuestro query: numero de usuarios en la base de datos que aparecen con el usuario ingresado en la pagina de registro
+            Dim crearReservacion As String = "Insert into reservaciones (id_reservations, userId, id_lugar, check_in, check_out, num_personas) values (@idres, @idus, @idlug,@chin,@chout,@pers)"
+
+            'definimos el commando a ejecutar y pasamos el query en el y la conexion a utiliar para ejecutar el comando
+            Dim command As SqlCommand = New SqlCommand(crearReservacion, conn)
 
 
-        '    'definimos la variable de conexion y el string de conexion a utilizar dentro de ella
-        '    Dim conn As New SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings("cs").ConnectionString)
+            'pasamos los parametros con los valores de la pagina de registro
+            command.Parameters.AddWithValue("@idres", id.ToString)
+            command.Parameters.AddWithValue("@idus", tbUserID.Text)
+            command.Parameters.AddWithValue("@idlug", tblugarID.Text)
+            command.Parameters.AddWithValue("@chin", calCheckIN.SelectedDate)
+            command.Parameters.AddWithValue("@chout", calCheckOUT.SelectedDate)
+            command.Parameters.AddWithValue("@pers", tbNumPersonas.Text)
 
 
-        '    conn.Open() ' abrimos la conexion
+            'ejecutados el query
+            command.ExecuteNonQuery()
 
+            MsgBox("el registro ha sido exitoso")
+            conn.Close() 'cerramos la conexion
+            'Server.Transfer("Default.aspx", True)
+            Response.BufferOutput = True
+            Response.Redirect("~\Default.aspx")
 
-        '    'definimos nuestro query: numero de usuarios en la base de datos que aparecen con el usuario ingresado en la pagina de registro
-        '    Dim insertarUsuario As String = "Insert into usuarios (id,nombre,apellido,usuario,clave,nivel_ingles,estado_civil,email) values(@id, @nom, @ape, @usu, @cla, @niv, @est, @ema)"
-
-
-        '    'definimos el commando a ejecutar y pasamos el query en el y la conexion a utiliar para ejecutar el comando
-        '    Dim command As SqlCommand = New SqlCommand(insertarUsuario, conn)
-
-
-        '    'pasamos los parametros con los valores de la pagina de registro
-        '    command.Parameters.AddWithValue("@id", id.ToString)
-        '    command.Parameters.AddWithValue("@nom", tbUserID.Text)
-        '    command.Parameters.AddWithValue("@ape", tblugarID.Text)
-        '    command.Parameters.AddWithValue("@usu", tbUsuario.Text)
-        '    command.Parameters.AddWithValue("@cla", tbClave.Text)
-        '    command.Parameters.AddWithValue("@niv", ddNivelIngles.SelectedValue.ToString)
-        '    command.Parameters.AddWithValue("@est", ddEstadoCivil.SelectedValue.ToString)
-        '    command.Parameters.AddWithValue("@ema", tbNumPersonas.Text)
-
-
-        '    'ejecutados el query
-
-
-        '    command.ExecuteNonQuery()
-
-
-        '    MsgBox("el registro ha sido exitoso")
-        '    Response.Redirect("ingreso.aspx")
-        '    conn.Close() 'cerramos la conexion
-
-
-        'Catch ex As Exception
-        '    MsgBox("error: " + ex.ToString())
-        'End Try
-
-
+        Catch ex As Exception
+            'MsgBox("error: " + ex.ToString())
+        End Try
 
     End Sub
 
